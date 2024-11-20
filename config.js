26b494a9f12474f590e20409ed7201afb471a232
TamPham147 <139139091+TamPham147@users.noreply.github.com>
2024-11-20 16:44:15 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,11 @@
+const nameGirl = '';
+const giftUrl = 'http://nodemy.vn';
+const eventName = '💘Happy Vietnamese Teachers Day💘 ';
+const titleCard = 'To Ms. Mia';
+const contentCard = 'On the occasion of Vietnamese Teachers\' Day November 20, I would like to sincerely thank you for your efforts in the past few days that have helped us progress in our studies. I wish you a happy and successful Teacher\'s Day. Happy Vietnamese Teachers\' Day!';
+
+
+
+const giftImage = 'mrsMia.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
